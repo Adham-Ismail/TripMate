7ff65c33def1f3975075e2e83fa4012b9795f4e3
AdhamIsmail <adhamhabdelaziz@gmail.com>
2026-06-21 15:53:53 +0300

    created first model - incomplete

---
+++ TripMate.DAL/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+//using Microsoft.AspNetCore.Identity;
+
+
+namespace TripMate.DAL.Models
+{
+    public class User : IdentityUser
+    {
+        string name;
+        string username;
+        string password;
+        string email;
+        string occupation;
+        string phone;
+        string city;
+        string country;
+        Shared.UserRole role;
+        string nid;
+        string dob;
+    }
+}
